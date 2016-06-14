3e23841250b14741fde65bad09db2a739e304e1a
Gabrielle Ong <gabrielleong97@gmail.com>
2016-06-14 18:41:47 +0800

    add solution 3 to leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,24 +1,31 @@
 var Year = function (input) {
-  this.isLeap = function(){
-    if (input % 4 === 0) {
-      if (input % 100 === 0) {
-        if (input % 400 === 0) {
-          return true;
-        } return false;
-      } return true;
-    } return false;
-  }
+  this.isLeap = function () {
+    // SOLUTION 1:
+    if (input % 400 === 0) {
+      return true;
+    }
+    else if (input % 4 === 0 && input % 100 !== 0) {
+      return true;
+    }
+    else return false;
 
-  // shorter alternative:
-  // must put else
+    //SOLUTION 2:
+    // if (input % 4 === 0) {
+    //   if (input % 100 === 0) {
+    //     if (input % 400 === 0) {
+    //       return true;
+    //     } return false;
+    //   } return true;
+    // } return false;
 
-  // if (input % 4 === 0) {
-  //   if (input % 100 === 0) {
-  //     if (input % 400 === 0){
-  //       return true;
-  //     }
-  //   } else return true;
-  // } return false;
-
-}
+    //SOLUTION 3: (SIMILAR TO 2)
+    // if (input % 4 === 0) {
+    //   if (input % 100 === 0) {
+    //     if (input % 400 === 0){
+    //       return true;
+    //     }
+    //   } else return true;
+    // } return false;
+  };
+};
 module.exports = Year;
