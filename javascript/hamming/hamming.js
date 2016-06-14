024f003ff29e17853bd2ef79db01a507d7c42448
Gabrielle Ong <gabrielleong97@gmail.com>
2016-06-14 11:22:15 +0800

    edited Leap & finished hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,13 +1,18 @@
-var Hamming = function () {
-  this.compute = function (string1, string2) {
-    var array1 = string1.split('');
-    var array2 = string2.split('');
-    var j = 0;
-    for (var i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        j += 1;
-      }
-    } return j;
-  };
+var Hamming = function () {}
+
+Hamming.prototype.compute = function (string1, string2) {
+  if (string1.length !== string2.length){
+    throw new Error('DNA strands must be of equal length.');
+  }
+  var array1 = string1.split('');
+  var array2 = string2.split('');
+  var j = 0;
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      j += 1;
+    }
+  }
+  return j;
 };
+
 module.exports = Hamming;
--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -2,11 +2,23 @@ var Year = function (input) {
   this.isLeap = function(){
     if (input % 4 === 0) {
       if (input % 100 === 0) {
-        if (input % 400 === 0){
+        if (input % 400 === 0) {
           return true;
         } return false;
       } return true;
     } return false;
   }
+
+  // shorter alternative:
+  // must put else
+
+  // if (input % 4 === 0) {
+  //   if (input % 100 === 0) {
+  //     if (input % 400 === 0){
+  //       return true;
+  //     }
+  //   } else return true;
+  // } return false;
+
 }
 module.exports = Year;
