92e2a8ea299af43d8121dbe3dd1d791e99cf4d19
Gabrielle Ong <gabrielleong97@gmail.com>
2016-06-15 10:58:36 +0800

    refactored code

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,18 +1,16 @@
+`use strict`
+
 var Hamming = function () {}
 
 Hamming.prototype.compute = function (string1, string2) {
-  if (string1.length !== string2.length){
-    throw new Error('DNA strands must be of equal length.');
+  count = 0
+  if (string1.length !== string2.length) {
+    throw Error('DNA strands must be of equal length.')
   }
-  var array1 = string1.split('');
-  var array2 = string2.split('');
-  var j = 0;
-  for (var i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      j += 1;
+  for (var i = 0; i < string1.length; i++) {
+    if (string1[i] !== string2[i]){
+      count += 1
     }
-  }
-  return j;
-};
-
+  } return count;
+}
 module.exports = Hamming;
