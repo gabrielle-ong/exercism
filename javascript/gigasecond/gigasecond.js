adcabff9b5e5e4f52accdfba512cd1c560dba731
Gabrielle Ong <gabrielleong97@gmail.com>
2016-06-17 02:34:27 +0800

    adds solution to gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,8 @@
-var Gigasecond = function (new Date(Date.UTC(year, month, day, hour, second))) {}
-
-Gigasecond.prototype.date = function () {
-  expectedDate = new Date(Date.UTC(2047, 4, 23, 1, 46, 40))
-  return expectedDate
+var Gigasecond = function (birthdate) {
+  this.date = function () {
+    var timePassed = birthdate.getTime() + 1000000000000
+    return new Date(timePassed)
+  }
 }
 
 module.exports = Gigasecond
