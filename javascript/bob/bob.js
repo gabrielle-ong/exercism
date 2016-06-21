c0ffc04d4ff256ca5b249b097febd3c3fd19ca65
Gabrielle Ong <gabrielleong97@gmail.com>
2016-06-21 10:57:47 +0800

    refactored solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,33 +1,15 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function () {}
 
 Bob.prototype.hey = function (input) {
   if (input.trim() === '') {
     return 'Fine. Be that way!'
   }
-
-  // all caps is most specific
-  // input === .toUpperCase input =>  'Whoa, chill out!'
-
-  // end with ? => 'Sure.'
-  // ALLCAPS? is shouting
-  // for whoa chill out, must test that contains letters if not 1,2,3 ; 4? ; and '   ' will return true
-  if (input.slice(-1) === '?') {
-    if (input.toUpperCase() === input && /[A-Z]/.test(input)) {
-      return 'Whoa, chill out!'
-    }
-    return 'Sure.'
-  }
-
   if (input.toUpperCase() === input && /[A-Z]/.test(input)) {
     return 'Whoa, chill out!'
   }
-
+  if (input.slice(-1) === '?') {
+    return 'Sure.'
+  }
   else return 'Whatever.'
 }
-
 module.exports = Bob
