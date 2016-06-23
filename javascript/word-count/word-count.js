0aa23b187eee798c8b75b7b028ee7bd9d20c3805
Gabrielle Ong <gabrielleong97@gmail.com>
2016-06-23 11:05:20 +0800

    word count refactored

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,17 +1,36 @@
 function Words () {}
 
 Words.prototype.count = function (string) {
-  var formattedString = string.trim().toLowerCase().replace(/\s+/g, ' ')
-  var sortedArray = formattedString.split(' ').sort()
-  var expectedCount = {}
+  var formattedArray = string.trim().toLowerCase().split(/\s+/)
+  var expectedCount = formattedArray.reduce(function (previous, current) {
+    previous.hasOwnProperty(current) ? previous[current]++ : previous[current] = 1
+    return previous
+  }, {})
 
-  for (var i = 0; i < sortedArray.length; i++) {
-    if (sortedArray[i] === sortedArray[i - 1]) {
-      expectedCount[sortedArray[i-1]] += 1
-    } else {
-      expectedCount[sortedArray[i]] = 1
-    }
-  }
+  // // SOLUTION 2
+  // var sortedArray = formattedArray.sort()
+  // var expectedCount = {}
+  //
+  // for (var i = 0; i < sortedArray.length; i++) {
+  //   if (sortedArray[i] === sortedArray[i - 1]) {
+  //     expectedCount[sortedArray[i - 1]] += 1
+  //   } else {
+  //     expectedCount[sortedArray[i]] = 1
+  //   }
+  // }
+
+  // // SOLUTION 3 (WITHOUT SORTING)
+  // var sortedArray = formattedArray.sort()
+  // var expectedCount = {}
+  //
+  // for (var i = 0; i < sortedArray.length; i++) {
+  // var j = sortedArray[i]
+  //   if (expectedCount[j] == null) {
+  //     expectedCount[j] = 1
+  //   } else {
+  //       expectedCount[j]++
+  //   }
+  // }
   return expectedCount
 }
 
