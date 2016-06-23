5a326fb09335820ae23149483420662ea35d6739
Gabrielle Ong <gabrielleong97@gmail.com>
2016-06-23 11:26:41 +0800

    adds @redswift's solution

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -3,10 +3,10 @@ function Words () {}
 Words.prototype.count = function (string) {
   // //SOLUTION 1 - credits to @giftofjehovah
   var formattedArray = string.trim().toLowerCase().split(/\s+/)
-  var expectedCount = formattedArray.reduce(function (previous, current) {
-    previous.hasOwnProperty(current) ? previous[current]++ : previous[current] = 1
-    return previous
-  }, {})
+  // var expectedCount = formattedArray.reduce(function (previous, current) {
+  //   previous.hasOwnProperty(current) ? previous[current]++ : previous[current] = 1
+  //   return previous
+  // }, {})
 
   // // SOLUTION 2 - credits to me!
   // var sortedArray = formattedArray.sort()
@@ -32,6 +32,20 @@ Words.prototype.count = function (string) {
   //       expectedCount[j]++
   //   }
   // }
+
+  // //SOLUTION 4 - credits to @redswift
+  // // Take the first index and compare with all other index, then second index and compare with all other indexes ...
+  var expectedCount = {}
+  for (var i = 0; i < formattedArray.length; i++) {
+   var counter = 0
+   console.log('i 1 ' + formattedArray[i])
+   for (var r = 0; r < formattedArray.length; r++) {
+     console.log('r 2 ' + formattedArray[r])
+     if (formattedArray[i] === formattedArray[r]) counter++
+     expectedCount[formattedArray[i]] = counter
+   }
+   console.log(expectedCount)
+ }
   return expectedCount
 }
 
