97c513ee3b7c4e4d0f1cf3d6b29675bbe2f2d3d4
Gabrielle Ong <gabrielleong97@gmail.com>
2016-07-14 11:59:48 +0800

    sieve

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,94 @@
+class Sieve
+  # def initialize(n)
+   #   @n = n
+   #   @answer = []
+   # end
+   # def primes
+   #   #SOLUTION 1: RECURSIVE FUNCTION
+   #   array = Array.new(@n+1, true)
+   #   for i in 2..@n
+   #     if array[i]
+   #       @answer << i
+   #       make_false(i+i, i , array)
+   #     end
+   #   end
+   #   @answer
+   # end
+   #
+   # private
+   # def make_false(current, increase, array)
+   #   if current <= @n
+   #     array[current] = false
+   #     make_false(current+increase, increase, array)
+   #   end
+   # end
+
+ ##SOLUTION 2
+   def initialize(n)
+     @n = n
+     @answer = []
+   end
+   def primes
+     array = Array.new(@n+1, true)
+     if @n < 2
+       return []
+     end
+     for i in 2..@n
+       if array[i]
+         @answer << i
+         multiple = i
+         while multiple < @n
+           multiple += i
+           array[multiple] = false
+         end
+       end
+       i+=1
+     end
+     return @answer
+   end
+ end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##SOLUTION 2
+  def initialize(n)
+    @n = n
+    @answer = []
+  end
+  def primes
+    array = Array.new(@n+1, true)
+    if @n < 2
+      return []
+    end
+    for i in 2..@n
+      if array[i]
+        @answer << i
+        multiple = i
+        while multiple < @n
+          multiple += i
+          array[multiple] = false
+        end
+      end
+      i+=1
+    end
+    return @answer
+  end
+end
