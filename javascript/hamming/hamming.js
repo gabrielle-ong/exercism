4e89f87c399488bd603df2b0f060b2830cdee78f
Gabrielle Ong <gabrielleong97@gmail.com>
2016-07-14 12:05:14 +0800

    replaced erroneous accessing string like an array

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -2,13 +2,15 @@
 var Hamming = function () {}
 
 Hamming.prototype.compute = function (string1, string2) {
+  count = 0
   if (string1.length !== string2.length) {
     throw Error('DNA strands must be of equal length.')
   }
   for (var i = 0; i < string1.length; i++) {
-    if (string1[i] !== string2[i]) {
+    if (string1.charAt(i) !== string2.charAt(i)) {
       count += 1
     }
-  } return count
+  }
+  return count
 }
 module.exports = Hamming
