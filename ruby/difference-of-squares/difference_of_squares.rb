9cdcc0c6070e02f99b0bfe1c0ad9284fef619e67
Gabrielle Ong <gabrielleong97@gmail.com>
2016-07-14 11:57:10 +0800

    refactored sol

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -3,15 +3,15 @@ class Squares
     @x = x
   end
   def square_of_sum
-    @square_sum = (0..@x).reduce(:+)**2
+    (0..@x).reduce(:+)**2
   end
 
   def sum_of_squares
-    @sum_square = (0..@x).reduce { |square, x| square + x**2 }
+    (0..@x).reduce { |square, x| square + x**2 }
   end
 
   def difference
     #square_of_sum always > sum_of_squares
-    difference = (square_of_sum) - (sum_of_squares)
+    difference = square_of_sum - sum_of_squares
   end
 end
